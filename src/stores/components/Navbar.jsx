825fc4546d5533b4cb4b22ccc78b4c66c27ff454
suchir-cyber <suchirpandula@gmail.com>
2024-05-28 02:37:28 +0530

    updated cart

--- src/stores/components/Navbar.jsx
+++ src/stores/components/Navbar.jsx
@@ -12,9 +12,13 @@ export const Navbar = () => {
         <div className="title">
             <h2>E-Mart</h2>
         </div>
-        <div className="search">
-            <input type="text" placeholder="Search..." size="50" />
+        <div class="search-container">
+        <input type="text" class="search-input" placeholder="Search..." />
+        <button class="search-button">Search</button>
         </div>
+        {/* <div className="search">
+            <input type="text" placeholder="Search..." size="50" />
+        </div> */}
         <div className="user">
             <div className="user-detail">SignIN/SignUp</div>
             <Link to='/cart'>
@@ -26,33 +30,37 @@ export const Navbar = () => {
             </Link>
         </div>
     </div>
-    <div className='subMenu'>
+
+
+    <div class="horizontal-scroll-container">
+    <div className='subMenu horizontal-content'>
        <ul>
           <Link to='/Rice'>
-            <li>Rice</li>
+          <div className='content-item'><li>Rice</li></div>
           </Link>
           <Link>
-            <li>Dal</li>
+          <div className='content-item'><li>Dal</li></div>
           </Link>
           <Link>
-            <li>Spices</li>
+          <div className='content-item'><li>Spices</li></div>
           </Link>
           <Link>
-            <li>Tea</li>
+          <div className='content-item'><li>Tea</li></div>
           </Link>
           <Link>
-            <li>Dry Fruits</li>
+          <div className='content-item'><li>Dry Fruits</li></div>
           </Link>
           <Link>
-            <li>Ghee</li>
+          <div className='content-item'><li>Ghee</li></div>
           </Link>
           <Link>
-            <li>Sugar</li>
+          <div className='content-item'><li>Sugar</li></div>
           </Link>
           <Link>
-            <li>Salt</li>
+          <div className='content-item'><li>Salt</li></div>
           </Link>
        </ul>
+    </div>
     </div>
     </div>
   )
